7e6c605185152165ac2712d79f6f01c1e6e95471
kam6512 <kam6512@gmail.com>
2017-02-26 03:56:05 +0900

    테스트 추가

--- rxpermission/src/test/java/com/orca/kam/rxpermission/ExampleUnitTest.java
+++ rxpermission/src/test/java/com/orca/kam/rxpermission/ExampleUnitTest.java
@@ -9,9 +9,11 @@ import java.util.List;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.ADD_VOICEMAIL;
 import static android.Manifest.permission.BODY_SENSORS;
 import static android.Manifest.permission.CALL_PHONE;
 import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.GET_ACCOUNTS;
 import static android.Manifest.permission.INTERNET;
 import static android.Manifest.permission.PROCESS_OUTGOING_CALLS;
 import static android.Manifest.permission.READ_CALENDAR;
@@ -59,6 +61,38 @@ public class ExampleUnitTest {
             WRITE_CALL_LOG,
             WRITE_CONTACTS,
             WRITE_EXTERNAL_STORAGE);
+    private List<String> exampleDuplicatePermissions = Lists.newArrayList(
+            READ_CALENDAR, WRITE_CALENDAR,
+            CAMERA,
+            READ_CONTACTS, WRITE_CONTACTS, GET_ACCOUNTS,
+            ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION,
+            RECORD_AUDIO,
+            READ_PHONE_STATE, CALL_PHONE,
+            READ_CALL_LOG, WRITE_CALL_LOG,
+            ADD_VOICEMAIL, USE_SIP,
+            PROCESS_OUTGOING_CALLS,
+            BODY_SENSORS,
+            SEND_SMS, RECEIVE_SMS, READ_SMS,
+            RECEIVE_WAP_PUSH, RECEIVE_MMS,
+            READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE,
+            INTERNET,
+            PROCESS_OUTGOING_CALLS,
+            READ_CALENDAR,
+            READ_CALL_LOG,
+            READ_CONTACTS,
+            READ_EXTERNAL_STORAGE,
+            READ_PHONE_STATE,
+            READ_SMS,
+            RECEIVE_MMS,
+            RECEIVE_SMS,
+            RECEIVE_WAP_PUSH,
+            RECORD_AUDIO,
+            SEND_SMS,
+            USE_SIP,
+            WRITE_CALENDAR,
+            WRITE_CALL_LOG,
+            WRITE_CONTACTS,
+            WRITE_EXTERNAL_STORAGE);
 
 
     @Test
@@ -66,12 +100,17 @@ public class ExampleUnitTest {
 
         System.out.println("TEST 1) : examplePermissions");
         showLog(examplePermissions);
-        System.out.println("TEST 2) : normalPermissions");
+        System.out.println("TEST 2) : filteringDangerousPermission");
         List<String> normalPermissions = PermissionUtil.filteringDangerousPermission(examplePermissions);
         showLog(normalPermissions);
-        System.out.println("TEST 3) : dangerousPermissions");
+        System.out.println("TEST 3) : filteringNormalPermission");
         List<String> dangerousPermissions = PermissionUtil.filteringNormalPermission(examplePermissions);
         showLog(dangerousPermissions);
+        System.out.println("TEST 4) : removeDuplicateStringInList");
+        showLog(exampleDuplicatePermissions);
+        System.out.println("TEST 5) : removeDuplicateStringInList");
+        List<String> removedDuplicateItemPermissions = PermissionUtil.removeDuplicateStringInList(exampleDuplicatePermissions);
+        showLog(removedDuplicateItemPermissions);
     }
 
 
