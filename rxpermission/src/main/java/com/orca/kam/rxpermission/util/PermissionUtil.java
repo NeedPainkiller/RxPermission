e81d034804aa3973b1be0c474eaa9ce59001f0d2
kam6512 <kam6512@gmail.com>
2017-02-26 02:31:59 +0900

    PermissionUtil 에 필터링 기능을 추가
    PermissionUtil.class :
        - 6.0 이전 버젼이라면 상관이 없겠지만, 퍼미션의 위험도에 따라 명시해줘야 할 퍼미션들이 있다.
        - 그러니깐 막 퍼미션 넣어놓고 돌려도 명시 할 필요없이 허락이 되는데 이게 또 Denial 로 뜨는 경우가 있어서 골때린다.
        - 각 메서드의 설명은 나중에 주석으로 올리고 테스트를 먼저 진행할 예정
    
    //    오랜만에 삽질했다, 안드로이드의 버젼별 대응책이 점점 고난이도(?) 되어가고 있다.
    //    그래도 이정도로 만들어준 엔지니어들에게 감사하자

--- rxpermission/src/main/java/com/orca/kam/rxpermission/util/PermissionUtil.java
+++ rxpermission/src/main/java/com/orca/kam/rxpermission/util/PermissionUtil.java
@@ -1,8 +1,13 @@
 package com.orca.kam.rxpermission.util;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import static android.Manifest.permission.*;
 
 /**
  * Project RxPermission
@@ -11,6 +16,78 @@ import java.util.Set;
  * @create 2017-02-21 - 오후 1:20
  */
 public class PermissionUtil {
+
+
+
+      /* // CALENDAR
+    String PERMISSION_READ_CALENDAR = READ_CALENDAR;
+    String PERMISSION_WRITE_CALENDAR = WRITE_CALENDAR;
+    //  CAMERA
+    String PERMISSION_CAMERA = CAMERA;
+    // CONTACTS
+    String PERMISSION_READ_CONTACTS = READ_CONTACTS;
+    String PERMISSION_WRITE_CONTACTS = WRITE_CONTACTS;
+    String PERMISSION_GET_ACCOUNTS = GET_ACCOUNTS;
+    //  LOCATION
+    String PERMISSION_ACCESS_FINE_LOCATION = ACCESS_FINE_LOCATION;
+    String PERMISSION_ACCESS_COARSE_LOCATION = ACCESS_COARSE_LOCATION;
+    //           MICROPHONE
+    String PERMISSION_RECORD_AUDIO = RECORD_AUDIO;
+    //            PHONE
+    String PERMISSION_READ_PHONE_STATE = READ_PHONE_STATE;
+    String PERMISSION_CALL_PHONE = CALL_PHONE;
+    String PERMISSION_READ_CALL_LOG = READ_CALL_LOG;
+    String PERMISSION_WRITE_CALL_LOG = WRITE_CALL_LOG;
+    String PERMISSION_ADD_VOICEMAIL = ADD_VOICEMAIL;
+    String PERMISSION_USE_SIP = USE_SIP;
+    String PERMISSION_PROCESS_OUTGOING_CALLS = PROCESS_OUTGOING_CALLS;
+    //          SENSORS
+    String PERMISSION_BODY_SENSORS = BODY_SENSORS;
+    //          SMS
+    String PERMISSION_SEND_SMS = SEND_SMS;
+    String PERMISSION_RECEIVE_SMS = RECEIVE_SMS;
+    String PERMISSION_READ_SMS = READ_SMS;
+    String PERMISSION_RECEIVE_WAP_PUSH = RECEIVE_WAP_PUSH;
+    String PERMISSION_RECEIVE_MMS = RECEIVE_MMS;
+    //STORAGE
+    String PERMISSION_READ_EXTERNAL_STORAGE = READ_EXTERNAL_STORAGE;
+    String PERMISSION_WRITE_EXTERNAL_STORAGE = WRITE_EXTERNAL_STORAGE;*/
+
+    private static List<String> dangerousPermissions = Lists.newArrayList(
+            READ_CALENDAR, WRITE_CALENDAR,
+            CAMERA,
+            READ_CONTACTS, WRITE_CONTACTS, GET_ACCOUNTS,
+            ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION,
+            RECORD_AUDIO,
+            READ_PHONE_STATE, CALL_PHONE,
+            READ_CALL_LOG, WRITE_CALL_LOG,
+            ADD_VOICEMAIL, USE_SIP,
+            PROCESS_OUTGOING_CALLS,
+            BODY_SENSORS,
+            SEND_SMS, RECEIVE_SMS, READ_SMS,
+            RECEIVE_WAP_PUSH, RECEIVE_MMS,
+            READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE);
+
+
+    public static boolean isDangerousPermission(String permission) {
+        for (String dangerousPermission : dangerousPermissions) {
+            if (dangerousPermission.equals(permission)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static List<String> filteringNormalPermission(List<String> permissions) {
+        return Lists.newArrayList(Iterables.filter(permissions, PermissionUtil::isDangerousPermission));
+    }
+
+
+    public static List<String> filteringDangerousPermission(List<String> permissions) {
+        return Lists.newArrayList(Iterables.filter(permissions, permission -> !isDangerousPermission(permission)));
+    }
+
 
     /**
      * @param o need to check object is empty
