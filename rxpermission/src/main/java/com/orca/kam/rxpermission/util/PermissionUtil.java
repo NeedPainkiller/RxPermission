93b5c15875288488fd84da821430e34917ababb8
kam6512 <kam6512@gmail.com>
2017-06-09 17:54:36 +0900

    PERMISSIONS 리스트 생성

--- rxpermission/src/main/java/com/orca/kam/rxpermission/util/PermissionUtil.java
+++ rxpermission/src/main/java/com/orca/kam/rxpermission/util/PermissionUtil.java
@@ -15,30 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-import static android.Manifest.permission.ADD_VOICEMAIL;
-import static android.Manifest.permission.BODY_SENSORS;
-import static android.Manifest.permission.CALL_PHONE;
-import static android.Manifest.permission.CAMERA;
-import static android.Manifest.permission.GET_ACCOUNTS;
-import static android.Manifest.permission.PROCESS_OUTGOING_CALLS;
-import static android.Manifest.permission.READ_CALENDAR;
-import static android.Manifest.permission.READ_CALL_LOG;
-import static android.Manifest.permission.READ_CONTACTS;
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
-import static android.Manifest.permission.READ_PHONE_STATE;
-import static android.Manifest.permission.READ_SMS;
-import static android.Manifest.permission.RECEIVE_MMS;
-import static android.Manifest.permission.RECEIVE_SMS;
-import static android.Manifest.permission.RECEIVE_WAP_PUSH;
-import static android.Manifest.permission.RECORD_AUDIO;
-import static android.Manifest.permission.SEND_SMS;
-import static android.Manifest.permission.USE_SIP;
-import static android.Manifest.permission.WRITE_CALENDAR;
-import static android.Manifest.permission.WRITE_CALL_LOG;
-import static android.Manifest.permission.WRITE_CONTACTS;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.Manifest.permission.*;
 import static android.content.pm.PackageManager.PERMISSION_DENIED;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS;
@@ -72,6 +49,147 @@ public class PermissionUtil {
             SEND_SMS, RECEIVE_SMS, READ_SMS,
             RECEIVE_WAP_PUSH, RECEIVE_MMS,
             READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE);
+
+
+    private static final List<String> PERMISSIONS = Lists.newArrayList(
+            ACCESS_CHECKIN_PROPERTIES,
+            ACCESS_COARSE_LOCATION,
+            ACCESS_FINE_LOCATION,
+            ACCESS_LOCATION_EXTRA_COMMANDS,
+            ACCESS_NETWORK_STATE,
+            ACCESS_NOTIFICATION_POLICY,
+            ACCESS_WIFI_STATE,
+            ACCOUNT_MANAGER,
+            ADD_VOICEMAIL,
+            BATTERY_STATS,
+            BIND_ACCESSIBILITY_SERVICE,
+            BIND_APPWIDGET,
+            BIND_CARRIER_MESSAGING_SERVICE,
+            BIND_CARRIER_SERVICES,
+            BIND_CHOOSER_TARGET_SERVICE,
+            BIND_CONDITION_PROVIDER_SERVICE,
+            BIND_DEVICE_ADMIN,
+            BIND_DREAM_SERVICE,
+            BIND_INCALL_SERVICE,
+            BIND_INPUT_METHOD,
+            BIND_MIDI_DEVICE_SERVICE,
+            BIND_NFC_SERVICE,
+            BIND_NOTIFICATION_LISTENER_SERVICE,
+            BIND_PRINT_SERVICE,
+            BIND_QUICK_SETTINGS_TILE,
+            BIND_REMOTEVIEWS,
+            BIND_SCREENING_SERVICE,
+            BIND_TELECOM_CONNECTION_SERVICE,
+            BIND_TEXT_SERVICE,
+            BIND_TV_INPUT,
+            BIND_VOICE_INTERACTION,
+            BIND_VPN_SERVICE,
+            BIND_VR_LISTENER_SERVICE,
+            BIND_WALLPAPER,
+            BLUETOOTH,
+            BLUETOOTH_ADMIN,
+            BLUETOOTH_PRIVILEGED,
+            BODY_SENSORS,
+            BROADCAST_PACKAGE_REMOVED,
+            BROADCAST_SMS,
+            BROADCAST_STICKY,
+            BROADCAST_WAP_PUSH,
+            CALL_PHONE,
+            CALL_PRIVILEGED,
+            CAMERA,
+            CAPTURE_AUDIO_OUTPUT,
+            CAPTURE_SECURE_VIDEO_OUTPUT,
+            CAPTURE_VIDEO_OUTPUT,
+            CHANGE_COMPONENT_ENABLED_STATE,
+            CHANGE_CONFIGURATION,
+            CHANGE_NETWORK_STATE,
+            CHANGE_WIFI_MULTICAST_STATE,
+            CHANGE_WIFI_STATE,
+            CLEAR_APP_CACHE,
+            CONTROL_LOCATION_UPDATES,
+            DELETE_CACHE_FILES,
+            DELETE_PACKAGES,
+            DIAGNOSTIC,
+            DISABLE_KEYGUARD,
+            DUMP,
+            EXPAND_STATUS_BAR,
+            FACTORY_TEST,
+            GET_ACCOUNTS,
+            GET_ACCOUNTS_PRIVILEGED,
+            GET_PACKAGE_SIZE,
+            GET_TASKS,
+            GLOBAL_SEARCH,
+            INSTALL_LOCATION_PROVIDER,
+            INSTALL_PACKAGES,
+            INSTALL_SHORTCUT,
+            INTERNET,
+            KILL_BACKGROUND_PROCESSES,
+            LOCATION_HARDWARE,
+            MANAGE_DOCUMENTS,
+            MASTER_CLEAR,
+            MEDIA_CONTENT_CONTROL,
+            MODIFY_AUDIO_SETTINGS,
+            MODIFY_PHONE_STATE,
+            MOUNT_FORMAT_FILESYSTEMS,
+            MOUNT_UNMOUNT_FILESYSTEMS,
+            NFC,
+            PACKAGE_USAGE_STATS,
+            PERSISTENT_ACTIVITY,
+            PROCESS_OUTGOING_CALLS,
+            READ_CALENDAR,
+            READ_CALL_LOG,
+            READ_CONTACTS,
+            READ_EXTERNAL_STORAGE,
+            READ_FRAME_BUFFER,
+            READ_INPUT_STATE,
+            READ_LOGS,
+            READ_PHONE_STATE,
+            READ_SMS,
+            READ_SYNC_SETTINGS,
+            READ_SYNC_STATS,
+            READ_VOICEMAIL,
+            REBOOT,
+            RECEIVE_BOOT_COMPLETED,
+            RECEIVE_MMS,
+            RECEIVE_SMS,
+            RECEIVE_WAP_PUSH,
+            RECORD_AUDIO,
+            REORDER_TASKS,
+            REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+            REQUEST_INSTALL_PACKAGES,
+            RESTART_PACKAGES,
+            SEND_RESPOND_VIA_MESSAGE,
+            SEND_SMS,
+            SET_ALARM,
+            SET_ALWAYS_FINISH,
+            SET_ANIMATION_SCALE,
+            SET_DEBUG_APP,
+            SET_PREFERRED_APPLICATIONS,
+            SET_PROCESS_LIMIT,
+            SET_TIME,
+            SET_TIME_ZONE,
+            SET_WALLPAPER,
+            SET_WALLPAPER_HINTS,
+            SIGNAL_PERSISTENT_PROCESSES,
+            STATUS_BAR,
+            SYSTEM_ALERT_WINDOW,
+            TRANSMIT_IR,
+            UNINSTALL_SHORTCUT,
+            UPDATE_DEVICE_STATS,
+            USE_FINGERPRINT,
+            USE_SIP,
+            VIBRATE,
+            WAKE_LOCK,
+            WRITE_APN_SETTINGS,
+            WRITE_CALENDAR,
+            WRITE_CALL_LOG,
+            WRITE_CONTACTS,
+            WRITE_EXTERNAL_STORAGE,
+            WRITE_GSERVICES,
+            WRITE_SECURE_SETTINGS,
+            WRITE_SETTINGS,
+            WRITE_SYNC_SETTINGS,
+            WRITE_VOICEMAIL);
 
 
     /**
